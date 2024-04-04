d888f14cda5b71a9f3b7236b75778e64c8517545
Carolinealt <73542400+Carolinealt@users.noreply.github.com>
2024-04-04 20:38:56 +0000

    Deploying to gh-pages from @ Carolinealt/goit-js-hw-11@63e4ae49272f87ae6393ddc8724f4c923d8f1d50 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,24 @@
-(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&s(o)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();
+(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))n(t);new MutationObserver(t=>{for(const r of t)if(r.type==="childList")for(const a of r.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&n(a)}).observe(document,{childList:!0,subtree:!0});function o(t){const r={};return t.integrity&&(r.integrity=t.integrity),t.referrerPolicy&&(r.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?r.credentials="include":t.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function n(t){if(t.ep)return;t.ep=!0;const r=o(t);fetch(t.href,r)}})();const i={key:"21174821-fd3fd6848262c16aee96184b8",q:"",image_type:"photo",orientations:"horizontal",safesearch:!0},{key:l,q:O,image_type:u,orientations:f,safesearch:m}=i;function d(s){return i.q=s,`https://pixabay.com/api/?key=${l}&q=${i.q}&image_type=${u}&orientation=${f}&safesearch=${m}`}function c(s){return fetch(`${d(s)}`).then(e=>e.json()).catch(e=>console.log(e))}const p=document.querySelector(".gallery");function h({webformatURL:s,tags:e,likes:o,views:n,comments:t,downloads:r}){return`<li class="gallery-item">
+  <img src="${s}" alt="${e}" class="image">
+  </img>
+
+        <ul class="stats-list">
+            <li class="stats-item">
+                <h3 class="stats-header">Likes</h3>
+                <p class="stats-p">${o}</p>
+            </li>
+            <li class="stats-item">
+                <h3 class="stats-header">Views</h3>
+                <p class="stats-p">${n}</p>
+            </li>
+            <li class="stats-item">
+                <h3 class="stats-header">Comments</h3>
+                <p class="stats-p">${t}</p>
+            </li>
+            <li class="stats-item">
+                <h3 class="stats-header">Downloads</h3>
+                <p class="stats-p">${r}</p>
+            </li>
+        </ul>
+</li>`}function y(s){s.then(({hits:e})=>e).then(e=>e.map(h)).then(e=>e.join("")).then(e=>p.insertAdjacentHTML("beforeend",e))}const g={form:document.querySelector(".main-form"),gallery:document.querySelector(".gallery")},{form:$,gallery:q}=g;$.addEventListener("submit",L);function L(s){s.preventDefault();const e=s.target.elements.request.value.trim();e&&b(e)}function b(s){q.innerHTML="";let e;c(s)&&(e=c(s),console.log("из меина",e),y(e))}
 //# sourceMappingURL=commonHelpers.js.map
